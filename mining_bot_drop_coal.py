d1238e676436c6aad0395b55d7158a9e138a13ae
Harbinger324 <119092936+Harbinger324@users.noreply.github.com>
2022-11-26 00:59:33 -0800

    Add files via upload

---
+++ mining_bot_drop_coal.py
@@ -0,0 +1,127 @@
+import pyautogui
+
+
+
+#this bot mines coal ore from the fally mining guild and drops it all
+#for maximum exp growth- goal is to get to 85
+
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#--------------------------------------------------
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#----------------------------------------------------
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#----------------------------------------------------
+
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#---------------------------------------------------
+
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+
+#-----------------------------------------------------
+
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#------------------------------------------------------
+
+#first click on coal ore
+pyautogui.click(x=405, y=450, duration = 13.33)
+
+#second click on coal ore
+pyautogui.click(x=357, y=475, duration = 13.33)
+
+#third click on coal ore
+pyautogui.click(x=296, y=427, duration = 13.33)
+
+#fourth click on coal ore
+pyautogui.click(x=460, y=433, duration = 13.33)
+
+#click on starting position
+pyautogui.click(x=404, y=500, duration = 13.33)
+
+#----now dropping the ore will start ----------------
+
+
+
